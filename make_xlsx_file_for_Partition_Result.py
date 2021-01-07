be0e1610a025d66f472016edddb537df88c990a0
Yeonkey <76510434+Yeonkey@users.noreply.github.com>
2021-01-07 14:26:28 +0900

    파티션결과 엑셀추합

---
+++ make_xlsx_file_for_Partition_Result.py
@@ -0,0 +1,30 @@
+import openpyxl
+
+wb = openpyxl.Workbook()
+sheet1 = wb.active
+cnt = 0
+Pnum = 110
+for j in range(10):
+    for i in range(10):
+        path = 'C:/Users/Yeonkey/Desktop/DBLAB/유전알고리즘/Tinkerd_MST inputfile/Partition_Result/EX'+str(j+1)+'/P'+str(j+1)+'10_100000_50_'+str(i)+'_result.txt'
+        f = open(path, "r")
+        lines = f.readlines()
+        time = lines[5][17:]
+        time = time.replace(" ","")
+        time = time.replace("\n","")
+        length = lines[6][20:]
+        length = length.replace(" ","")
+        length = length.replace("\n","")
+        sheet1.cell(row = 1, column = cnt+1,value = Pnum)
+        sheet1.cell(row = 1, column = cnt+2,value = 'time')
+        sheet1.cell(row = 1, column = cnt+3,value = 'length')
+        sheet1.cell(row = i+2, column = cnt+2,value = float(time))
+        sheet1.cell(row = i+2, column = cnt+3,value = float(length))
+
+
+    cnt = cnt+4
+    Pnum = Pnum+100
+wb.save('C:/Users/Yeonkey/Desktop/DBLAB/유전알고리즘/Tinkerd_MST inputfile/Partition_Result/PARTITION_RESULT.xlsx')
+
+wb.close()
+
